1a52b0befbcb7cb8cf7fd2f6719899b93d6dd4ba
Piotr Przechlewski <haesor@gmail.com>
2018-01-30 18:00:10 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ var a = 5;
 h = 6;
 TriangleArea = a * h / 2;
 function getTriangleArea(a, h) {
-    if (a < 0);
+    if (a < 0)
     {
         alert("Nieprawid³owe dane");
     }
-    if (h < 0);
+    if (h < 0)
     {
         alert("Nieprawidlowe dane");
     }
 else
 {
 return "TriangleArea";
+}
 }
 console.log(getTriangleArea(10, 6));
